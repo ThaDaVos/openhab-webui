4d4a06f9e33716d57d9ab4e5c77bff97a8a9f324
Dylan Vos <dylan@vos.email>
2025-01-13 11:55:46 +0100

    Installed `Framework7`, `Framework7-icons` and `Framework7-Vue` and configured it
    Signed-off-by: Dylan Vos <dylan@vos.email> (github: ThaDaVos)

--- bundles/org.openhab.ui/web/src/main.ts
+++ bundles/org.openhab.ui/web/src/main.ts
@@ -3,12 +3,24 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Import Framework7 Core
+import Framework7 from 'framework7/lite-bundle';
+
+// Import Framework7 Vue
+// @ts-expect-error registerComponents is missing from the typescript definition
+import Framework7Vue, { registerComponents } from 'framework7-vue/bundle'
+
 import App from './App.vue'
 import router from './router'
+
+// Init plugin
+Framework7.use(Framework7Vue)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+registerComponents(app);
 
 app.mount('#app')
